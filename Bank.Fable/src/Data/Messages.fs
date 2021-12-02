a804fb9109d89cfeb1b234cbe5c2a42a34bbc796
Rune Lund-Søltoft <rune.lund.soltoft@criipto.com>
2021-12-02 19:27:59 +0100

    adding CORS header for netlify deployment of messages.json

--- Bank.Fable/src/Data/Messages.fs
+++ Bank.Fable/src/Data/Messages.fs
@@ -6,25 +6,44 @@ open Feliz.Bulma
 type private Message = Fable.JsonProvider.Generator<"../public/messages.json">
 
 let mutable messages = []
+let sources = 
+  [
+      "https://agitated-villani-6f8525.netlify.app/messages.json"
+     // "/messages.json"
+  ]
 let fetch(setMessages) = 
-    async {
-        if messages |> List.isEmpty then
-            let! (statusCode,messagesRaw) = Fable.SimpleHttp.Http.get "/messages.json"
-            if statusCode = 200 then
-                messages <- 
-                    Message(messagesRaw).messages
-                    |> Array.mapi(fun i m ->
-                        {
-                            Subject = m.title
-                            Content = m.content
-                            From = m.from
-                            Date = System.DateTime.Parse(m.date,System.Globalization.CultureInfo.InvariantCulture)
-                            Unread = (i % 5 = 1)
-                        } : Models.Message
-                    ) |> Array.sortByDescending(fun m ->
-                        (if m.Unread then 1 else 0),m.Date
-                    ) |> List.ofArray
-                if messages |> List.isEmpty |> not then setMessages messages
-            else
-                eprintfn "Failed to retrieve messages %d %s" statusCode messagesRaw
-    } |> Async.StartImmediate
+    let rec inner srcs = 
+        match srcs with
+        src::s ->
+            async {
+                let! (statusCode,messagesRaw) = Fable.SimpleHttp.Http.get src
+                if statusCode = 200 then
+                    messages <- 
+                        Message(messagesRaw).messages
+                        |> Array.mapi(fun i m ->
+                            {
+                                Subject = m.title
+                                Content = m.content
+                                From = m.from
+                                Date = System.DateTime.Parse(m.date,System.Globalization.CultureInfo.InvariantCulture)
+                                Unread = (i % 5 = 1)
+                            } : Models.Message
+                        ) |> Array.sortByDescending(fun m ->
+                            (if m.Unread then 1 else 0),m.Date
+                        ) |> List.ofArray
+                    if messages |> List.isEmpty |> not then 
+                        setMessages messages                    
+                    else 
+                        return! inner s
+                else
+                    eprintfn "Failed to retrieve messages %d %s from %s" statusCode messagesRaw src
+                    return! inner s
+            }
+        | [] -> 
+            eprintfn "Failed to retrieve messages from any source"
+            async { return ()}
+
+    if messages |> List.isEmpty then
+        sources
+        |> inner 
+        |> Async.StartImmediate
