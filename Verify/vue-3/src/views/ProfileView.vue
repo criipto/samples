4f8ff5240e34e509128d24ead270b61b6fa32a66
nmoskaleva <natalia.moskaleva@criipto.com>
2023-03-07 19:04:17 +0100

    fix typo

--- Verify/vue-3/src/views/ProfileView.vue
+++ Verify/vue-3/src/views/ProfileView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import criiptoAuth from '../../criipto-auth'
-import { reactive, onMounted } from 'vue/dist/vue';
+import { reactive, onMounted } from 'vue';
 
 const state = reactive({
   userName: 'user',
