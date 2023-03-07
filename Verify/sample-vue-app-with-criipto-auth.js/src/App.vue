63493468fcfd62bcf3696399c0fd7b88a5816cbf
nmoskaleva <natalia.moskaleva@criipto.com>
2023-03-07 17:59:09 +0100

    add sample Vue 3 app

---
+++ Verify/sample-vue-app-with-criipto-auth.js/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/profile">Profile</RouterLink>
+        </nav>
+      </div>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
+  z-index: 2;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 6rem;
+  padding-top: 1rem;
+  font-size: 1.1rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ Verify/sample-vue-app-with-criipto-auth.js/src/views/ProfileView.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import criiptoAuth from '../../criipto-auth'
+import { reactive, onMounted } from 'vue';
+
+const state = reactive({
+  userName: 'user',
+  isLoggedIn: false,
+  claims: JSON.parse(sessionStorage.getItem('claims') || '{}'),
+});
+
+function matchCallback() {
+  criiptoAuth.redirect
+    .match()
+    .then((result) => {
+      if (result && result.claims) {
+        state.isLoggedIn = true;
+        state.claims = result.claims;
+        state.userName = result.claims.name;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+onMounted(() => {
+    matchCallback();
+});
+
+const logout = () => {
+  criiptoAuth.logout({
+    redirectUri: 'http://localhost:5173',
+  });
+  sessionStorage.removeItem('claims');
+};
+
+</script>
+
+<template>
+  <div class="profile">
+    <div v-if="state.isLoggedIn">
+      <h1> 
+        Hello, <span>{{ state.userName }}</span>
+      </h1>
+      <div class="content">
+        <div class="left">
+          <h2>You are logged in</h2>
+          <button @click="logout">Logout</button>
+        </div>
+        <div class="right">
+          <ul>
+            <li v-for="(value, key) in state.claims" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h2>Please login on the Home page</h2>
+    </div>
+  </div>
+</template>
+
+<style>
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px; 
+  position: relative;
+  top: 50px;
+  left: 0;
+  z-index: 0;
+}
+
+h1 {
+  font-size: 2.5rem;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  max-width: 1200px;
+}
+
+.left {
+  margin-left: 2.5rem;
+
+}
+
+.right {
+  flex: 1;
+  margin-left: 2rem;
+  overflow-y: scroll
+}
+
+h2 {
+  font-size: 2rem;
+  font-weight: 600;
+  margin-bottom: 6.5rem;
+}
+
+span {
+  color: #0070f3;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+  line-height: 1.5;
+  border-bottom: 1px solid #eaeaea;
+  padding-bottom: 0.5rem;
+}
+
+li:last-of-type {
+  border-bottom: none;
+}
+
+</style>
