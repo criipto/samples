b61d1292015e855b0a0c9ae83d7ea1a3bb838fc6
nmoskaleva <natalia.moskaleva@criipto.com>
2023-05-24 10:26:47 +0200

    Use Node.js SDK

--- Signing/nodejs/src/sample.ts
+++ Signing/nodejs/src/sample.ts
@@ -1,5 +1,4 @@
-import { GraphQLClient } from 'graphql-request';
-import { getSdk, DocumentStorageMode, SignatoryStatus, SignatureOrderQuery } from './graphql';
+import CriiptoSignatures from '@criipto/signatures';
 
 import {readFileSync, writeFileSync} from 'fs';
 
@@ -7,21 +6,15 @@ const samplePdf = readFileSync(__dirname + '/../input-sample.pdf');
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) throw new Error('Environment variables CLIENT_ID and CLIENT_SECRET must be defined. They are available when you create a signature application on https://manage.criipto.id');
 
-const client = new GraphQLClient('https://signatures-api.criipto.com/v1/graphql', {
-  headers: {
-    Authorization: `Basic ${Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')}`
-  }
-});
-const sdk = getSdk(client);
+const client = new CriiptoSignatures(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
-async function pollSignatureOrderComplete(id: string, delay = 5000) : Promise<SignatureOrderQuery["signatureOrder"]> {
+async function pollSignatureOrderComplete(id: string, delay = 5000) : ReturnType<typeof client["querySignatureOrder"]> {
   console.log('Polling signature order ...');
-  const signatureOrder = await sdk.signatureOrder({
-    id
-  }).then(result => result.signatureOrder!);
 
-  const allComplete = signatureOrder.signatories.reduce((complete, signatory) => complete && signatory.status != SignatoryStatus.Open, true);
+  const signatureOrder = await client.querySignatureOrder(id).then(result => result!);
+  const allComplete = signatureOrder?.signatories.reduce((complete, signatory) => complete && signatory.status != 'OPEN', true);
   if (allComplete) return signatureOrder;
+  
   await new Promise((resolve) => setTimeout(resolve, delay));
   return await pollSignatureOrderComplete(id, delay);
 }
@@ -29,27 +22,22 @@ async function pollSignatureOrderComplete(id: string, delay = 5000) : Promise<Si
 async function main() {
   console.log('Creating signature order ...');
 
-  const createdSignatureOrder = await sdk.createSignatureOrder({
-    input: {
-      documents: [
-        {
-          pdf: {
-            title: 'Sample',
-            storageMode: DocumentStorageMode.Temporary,
-            blob: samplePdf.toString('base64')
-          }
+  const createdSignatureOrder = await client.createSignatureOrder({
+    title: "Sample",
+    documents: [
+      {
+        pdf: {
+          title: "Node.js Sample",
+          blob: Buffer.from(samplePdf), 
+          storageMode: 'Temporary' 
         }
-      ]
-    }
-  }).then(result => result.createSignatureOrder?.signatureOrder);
+      }
+    ]
+  })
   if (!createdSignatureOrder) throw new Error('Failed adding signatureOrder');
 
   console.log('Adding signatory ...');
-  const signatory = await sdk.addSignatory({
-    input: {
-      signatureOrderId: createdSignatureOrder.id
-    }
-  }).then(result => result.addSignatory?.signatory);
+  const signatory = await client.addSignatory(createdSignatureOrder.id);
   if (!signatory) throw new Error('Failed adding signatory');
 
   console.log(`Signatory added, open ${signatory.href} to sign documents`);
@@ -58,20 +46,19 @@ async function main() {
   const polledSignatureOrder = await pollSignatureOrderComplete(createdSignatureOrder.id);
   
   polledSignatureOrder?.signatories.forEach(signatory => {
-    if (signatory.status === SignatoryStatus.Error || signatory.status === SignatoryStatus.Rejected) {
+    if (signatory.status === 'ERROR' || signatory.status === 'REJECTED') {
       throw new Error('A signatory failed or declined to sign');
     }
   });
 
-  const closedSignatureOrder = await sdk.closeSignatureOrder({
-    input: {
-      signatureOrderId: createdSignatureOrder.id
-    }
-  }).then(result => result.closeSignatureOrder?.signatureOrder);
+  const closedSignatureOrder = await client.closeSignatureOrder(
+    createdSignatureOrder.id
+  );
   if (!closedSignatureOrder) throw new Error('Failed closing signatureOrder');
 
   closedSignatureOrder.documents.forEach((document, index) => {
-    writeFileSync(index ? __dirname + `/../output-sample-${index}.pdf` : __dirname + `/../output-sample.pdf`, Buffer.from(document.blob, 'base64'));
+    document.blob && 
+      writeFileSync(index ? __dirname + `/../output-sample-${index}.pdf` : __dirname + `/../output-sample.pdf`, Buffer.from(document.blob));
   });
 
   console.log('Signature order signed and closed, inspect result in output-sample.pdf');
--- Signing/nodejs/src/selectively-enabled-drawable.ts
+++ Signing/nodejs/src/selectively-enabled-drawable.ts
@@ -1,5 +1,4 @@
-import { GraphQLClient } from 'graphql-request';
-import { getSdk, DocumentStorageMode, SignatoryStatus, SignatureOrderQuery } from './graphql';
+import CriiptoSignatures from '@criipto/signatures';
 
 import {readFileSync, writeFileSync} from 'fs';
 
@@ -7,70 +6,58 @@ const samplePdf = readFileSync(__dirname + '/../input-sample.pdf');
 
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) throw new Error('Environment variables CLIENT_ID and CLIENT_SECRET must be defined. They are available when you create a signature application on https://manage.criipto.id');
 
-const client = new GraphQLClient('https://signatures-api.criipto.com/v1/graphql', {
-  headers: {
-    Authorization: `Basic ${Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')}`
-  }
-});
-const sdk = getSdk(client);
+const client = new CriiptoSignatures(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
 async function main() {
   console.log('Creating signature order ...');
 
-  const createdSignatureOrder = await sdk.createSignatureOrder({
-    input: {
-      documents: [
-        {
-          pdf: {
-            title: 'Sample',
-            storageMode: DocumentStorageMode.Temporary,
-            blob: samplePdf.toString('base64')
-          }
+  const createdSignatureOrder = await client.createSignatureOrder({
+    title: "Sample",
+    documents: [
+      {
+        pdf: {
+          title: "Node.js Sample",
+          blob: Buffer.from(samplePdf), 
+          storageMode: 'Temporary' 
         }
-      ],
-      evidenceProviders: [
-          {
-              enabledByDefault: true,
-              criiptoVerify: {
-                  uniqueEvidenceKey: "sub"
-              }
-          },
-          {
-              enabledByDefault: false,
-              drawable: {
-                  requireName: true
-              }
-          }
-      ]
-    }
-  }).then(result => result.createSignatureOrder?.signatureOrder);
+      }
+    ],
+    evidenceProviders: [
+      {
+        enabledByDefault: true,
+        criiptoVerify: {
+          uniqueEvidenceKey: "sub"
+        }
+      },
+      {
+        enabledByDefault: false,
+        drawable: {
+          requireName: true
+        }
+      }
+    ]
+  });
   if (!createdSignatureOrder) throw new Error('Failed adding signatureOrder');
 
-  const verifyEvidenceProvider = createdSignatureOrder.evidenceProviders.find(s => s.__typename === 'CriiptoVerifySignatureEvidenceProvider')!;
-  const drawableEvidenceProvider = createdSignatureOrder.evidenceProviders.find(s => s.__typename === 'DrawableSignatureEvidenceProvider')!;
+  const verifyEvidenceProvider = createdSignatureOrder!.evidenceProviders.find(s => s.__typename === 'CriiptoVerifySignatureEvidenceProvider')!;
+  const drawableEvidenceProvider = createdSignatureOrder!.evidenceProviders.find(s => s.__typename === 'DrawableSignatureEvidenceProvider')!;
 
   console.log('Adding signatories ...');
   const [verifySignatory, drawableSignatory] = await Promise.all([
-    await sdk.addSignatory({
-      input: {
-        signatureOrderId: createdSignatureOrder.id,
-        evidenceProviders: [
-          {
-            id: verifyEvidenceProvider.id
-          }
-        ]
-      }
-    }).then(result => result.addSignatory?.signatory),
-    await sdk.addSignatory({
-      input: {
-        signatureOrderId: createdSignatureOrder.id,
-        evidenceProviders: [
-          {
-            id: drawableEvidenceProvider.id
-          }
-        ]
-      }
-    }).then(result => result.addSignatory?.signatory)
+    await client.addSignatory(createdSignatureOrder.id, {
+      evidenceProviders: [
+        {
+          id: verifyEvidenceProvider!.id
+        }
+      ]
+    }),
+    await client.addSignatory(createdSignatureOrder.id, {
+      evidenceProviders: [
+        {
+          id: drawableEvidenceProvider!.id
+        }
+      ]
+    })
   ])
   if (!verifySignatory || !drawableSignatory) throw new Error('Failed adding signatories');
 
